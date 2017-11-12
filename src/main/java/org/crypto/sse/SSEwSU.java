de91d837a08af2619d665bddcd2633d3c8139f6b
Sorin Vatasoiu <sorin_vatasoiu@brown.edu>
2017-11-12 03:37:16 -0500

    Basic SSEwSU over finite field. not using EC yet

--- src/main/java/org/crypto/sse/SSEwSU.java
+++ src/main/java/org/crypto/sse/SSEwSU.java
@@ -16,42 +16,345 @@
 
 package org.crypto.sse;
 
+import java.io.IOException;
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.SecureRandom;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+
+import java.util.Set;
+
+import javax.crypto.NoSuchPaddingException;
+
+import com.google.common.collect.Multimap;
 
 public class SSEwSU {
-	
-	class Server implements Serializable {
-	
-		//state: set of encrypted documents
-		//some kind of keys for searching
-		//authorization set
+
+	public final static int securityParameter = 128;
+	public final static int idLength = 128;
+	public final BigInteger fieldOrder;
+
+	private Server server;
+	private Manager manager;
+
+	public SSEwSU(Multimap<String, String> mm) 
+			throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+
+		// TODO select random group generator + set up EC
+		// TODO genericize ^ to allow for arbitrary rewritable-hash
+		fieldOrder = BigInteger.probablePrime(securityParameter, new SecureRandom());
 		
-		//public lotsofkeydataformanager Setup(Set<Document);
-		//public Set<Document> SearchReply(qSet); (takes query ciphertexts from SearchQuery)
-		//AuthComputingUpdate: takes some output from AuthComputing and updates data to reflect new access permissions
-		
+		this.server = new Server();
+		this.manager = new Manager();
+		this.manager.setup(this.server, mm);
 	}
-	
+
+	public void enroll(String username) throws UserAlreadyExists {
+		this.manager.enroll(username);
+	}
+
+	public void shareDoc(String documentName, String username) throws UserDoesntExist, DocumentDoesntExist, UnsupportedEncodingException {
+		this.manager.shareDoc(documentName, username);
+	}
+
+	public void unshareDoc(String documentName, String username) throws UserDoesntExist, DocumentDoesntExist, UnsupportedEncodingException {
+		this.manager.unshareDoc(documentName, username);
+	}
+
+	public Collection<String> query(String username, String keyword) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+		if (!this.manager.users.containsKey(username)) {
+			System.out.println("User " + username + " does not exist");
+			return null;
+		}
+		return this.manager.users.get(username).search(keyword);
+	}
+
+	public byte[] F(byte[] key, byte[] x) throws UnsupportedEncodingException { 
+		// must be in F_p for prime p
+		BigInteger tmp = new BigInteger(CryptoPrimitives.generateHmac(key, x));
+		byte[] result = tmp.mod(fieldOrder).toByteArray(); 
+		System.out.println("F(" + Arrays.toString(key) + ", " + Arrays.toString(x) + ") = " + Arrays.toString(result));
+		return result;
+	}
+
+	public byte[] G(byte[] key, byte[] x) throws UnsupportedEncodingException { 
+		byte[] result = CryptoPrimitives.generateHmac(key, x);
+		System.out.println("G(" + Arrays.toString(key) + ", " + Arrays.toString(x) + ") = " + Arrays.toString(result));
+		return result;
+	}
+
+	public byte[] Encrypt(byte[] key, String x) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException { 
+		return CryptoPrimitives.encryptAES_CTR_String(key, CryptoPrimitives.randomBytes(16), x, 40);
+	}
+
+	public String Decrypt(byte[] key, byte[] x) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException { 
+		return new String(CryptoPrimitives.decryptAES_CTR_String(x, key)); 
+	}
+
+	static class Query {
+		byte[] authTokenID;
+		byte[] queryCiphertext;
+
+		Query(byte[] tokID, byte[] ct) {
+			this.authTokenID = tokID;
+			this.queryCiphertext = ct;
+		}
+	}
+
+	class Server {
+		
+		// /home/sorin/tmp/clusion_test
+		// /home/sorin/tmp/simple_test
+
+		// map: EncryptedDocumentWordPair -> EncryptedDocumentMetadata
+		Map<ByteBuffer, byte[]> encryptedMM;
+		// map: authTokenID -> authToken
+		Map<ByteBuffer, byte[]> authTokenMap;
+
+		Server() { }
+
+		public void setup(Map<ByteBuffer, byte[]> encryptedMM) {
+			this.encryptedMM = encryptedMM;
+			this.authTokenMap = new HashMap<ByteBuffer, byte[]>();
+		}
+
+		public Set<byte[]> search(Set<Query> queries) {
+			Set<byte[]> resultSet = new HashSet<byte[]>();
+			for (Query query : queries) {
+				System.out.println("Looking for AuthTokenID: " + Arrays.toString(query.authTokenID));
+				byte[] authToken = authTokenMap.get(ByteBuffer.wrap(query.authTokenID));
+				if (authToken == null) {
+					continue;
+				}
+
+				BigInteger tmp = new BigInteger(query.queryCiphertext);
+				BigInteger tmp2 = new BigInteger(authToken);
+				byte[] xCT = (tmp.multiply(tmp2)).mod(fieldOrder).toByteArray();
+				byte[] yCT = encryptedMM.get(ByteBuffer.wrap(xCT));
+				if (yCT != null)
+					resultSet.add(yCT);
+			}
+			return resultSet;
+		}
+
+		public void giveAccess(byte[] authTokID, byte[] authTok) {
+			System.out.println("Added " + Arrays.toString(authTokID) + " to access list");
+			authTokenMap.put(ByteBuffer.wrap(authTokID), authTok);
+		}
+
+		public void removeAccess(byte[] authTokID) {
+			authTokenMap.remove(ByteBuffer.wrap(authTokID));
+		}
+
+	}
+
 	class Manager {
-		
-		//state: 3 master keys {0, 1}^lambda
-		//maybe index for document ids to share
-		
-		//constructor: takes lotsofkeydataformanager from server setup
-		//public UserKeys Enroll(user)
-		//AuthComputing: share d with u; takes user+user's keys, document id d, master keys (in manager state); sends result to server
-		
+
+		Map<String, User> users;
+		private Map<String, DocumentInfo> documents;
+		private byte[][] masterKeys;
+		private Server server;
+
+		Manager() {
+			users = new HashMap<String, User>();
+			documents = new HashMap<String, DocumentInfo>();
+			masterKeys = new byte[3][];
+		}
+
+		public void setup(Server server, Multimap<String, String> mm) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+			this.server = server;
+
+			// select 3 master keys
+			for (int i = 0; i < 3; ++i)
+				masterKeys[i] = CryptoPrimitives.randomBytes(securityParameter);
+
+			// set up encrypted map
+			Map<ByteBuffer, byte[]> encryptedMM = new HashMap<ByteBuffer, byte[]>();
+			for (String word : mm.keySet()) {
+				for (String documentName : mm.get(word)) {
+					if (!documents.containsKey(documentName)) {
+						byte[] docID = documentNameToID(documentName);
+						documents.put(documentName, 
+								new DocumentInfo(docID,
+										F(masterKeys[0], docID),
+										F(masterKeys[1], docID),
+										G(masterKeys[2], docID)));
+					}
+					DocumentInfo document = documents.get(documentName);
+					// TODO use EC generator
+					BigInteger tmp = new BigInteger(F(document.Kd2, document.documentID));
+					BigInteger tmp2 = new BigInteger(F(document.Kd1, wordToID(word)));
+					byte[] xCT = (tmp.multiply(tmp2)).mod(fieldOrder).toByteArray();
+					byte[] yCT = Encrypt(document.encKey, documentName);
+					encryptedMM.put(ByteBuffer.wrap(xCT), yCT);
+				}
+			}
+
+			// TODO randomize order of map (?)
+
+			// send encrypted map to server
+			this.server.setup(encryptedMM);
+		}
+
+		public User enroll(String username) throws UserAlreadyExists {
+			if (users.containsKey(username)) {
+				throw new UserAlreadyExists(username);
+			}
+
+			// generate keys for user
+			User newUser = new User(username, new byte[][] {
+				CryptoPrimitives.randomBytes(securityParameter),
+				CryptoPrimitives.randomBytes(securityParameter)
+			}, this.server);
+			users.put(username, newUser);
+			return null;
+		}
+
+		public void shareDoc(String documentName, String username) throws UserDoesntExist, DocumentDoesntExist, UnsupportedEncodingException {
+			if (!users.containsKey(username)) 
+				throw new UserDoesntExist(username);
+
+			if (!documents.containsKey(documentName))
+				throw new DocumentDoesntExist(documentName);
+
+			User user = users.get(username);
+			DocumentInfo document = documents.get(documentName);
+
+			// compute authorization token
+			BigInteger tmp = new BigInteger(F(document.Kd2, document.documentID));
+			BigInteger tmp2 = new BigInteger(F(user.userKeys[0], document.documentID));
+			byte[] authToken = (tmp.multiply(tmp2.modInverse(fieldOrder))).mod(fieldOrder).toByteArray();
+
+			// compute authorization token id
+			byte[] authTokenID = F(user.userKeys[1], document.documentID);
+
+			// send (token id, token) to server
+			this.server.giveAccess(authTokenID, authToken);
+
+			// send documentID, Kd, and Kd^enc to user
+			user.addDocumentInfo(document.documentID, document.Kd1, document.encKey);
+		}
+
+		public void unshareDoc(String documentName, String username) throws UserDoesntExist, DocumentDoesntExist, UnsupportedEncodingException {
+			if (!users.containsKey(username)) 
+				throw new UserDoesntExist(username);
+
+			if (!documents.containsKey(documentName))
+				throw new DocumentDoesntExist(documentName);
+
+			User user = users.get(username);
+			DocumentInfo document = documents.get(documentName);
+
+			byte[] authTokenID = F(user.userKeys[1], document.documentID);
+			this.server.removeAccess(authTokenID);
+		}
+
 	}
-	
+
 	class User {
-		
-		//state: id
-		//two user keys {0, 1}^lambda
-		//keys for documents shared
-		
-		//constructor: takes UserKeys from manager Enroll
-		//SearchQuery: produce set of query ciphertexts for keyword w on all documents user accesses
-		//AuthComputingUpdate: takes some output from AuthComputing and updates data to allow access
+
+		private String username;
+		private byte[][] userKeys;
+		private List<DocumentInfo> accessList;
+		private Server server;
+
+		public User(String username, byte[][] keys, Server server) {
+			this.username = username;
+			this.userKeys = keys;
+			this.accessList = new ArrayList<DocumentInfo>();
+			this.server = server;
+		}
+
+		public Collection<String> search(String keyword) throws InvalidKeyException, InvalidAlgorithmParameterException, NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, IOException {
+			Set<Query> queryCiphertexts = new HashSet<>();
+			for (DocumentInfo document : this.accessList) {
+				// compute auth token
+				byte[] authTokenID = F(userKeys[1], document.documentID);
+				// compute query ciphertext
+				// TODO USE EC
+				BigInteger tmp = new BigInteger(F(document.Kd1, wordToID(keyword)));
+				BigInteger tmp2 = new BigInteger(F(userKeys[0], document.documentID));
+				byte[] queryCT = (tmp.multiply(tmp2)).mod(fieldOrder).toByteArray();
+
+				queryCiphertexts.add(new Query(authTokenID, queryCT));
+			}
+
+			// send query ciphertext to server
+			Set<byte[]> queryResponse = this.server.search(queryCiphertexts);
+
+			// decrypt response from server
+			Collection<String> result = new ArrayList<String>();
+			for (byte[] encryptedDocMetadata : queryResponse) {
+				// HOW TO KNOW WHICH DOCUMENT A RESPONSE IS FOR (in order to decrypt it w/ the right key)
+				// need to return docID too?
+				// FOR NOW: ASSUME JUST 1 FILE
+				result.add(Decrypt(accessList.get(0).encKey, encryptedDocMetadata));
+			}
+			return result;
+		}
+
+		public void addDocumentInfo(byte[] docID, byte[] docKey, byte[] encKey) {
+			this.accessList.add(new DocumentInfo(docID, docKey, null, encKey));
+		}
+
+	}
+
+	static class DocumentInfo {
+		private byte[] documentID;
+		private byte[] Kd1;
+		private byte[] Kd2;
+		private byte[] encKey;
+
+		DocumentInfo(byte[] docID, byte[] kd1, byte[] kd2, byte[] encKey) {
+			this.documentID = docID;
+			this.Kd1 = kd1;
+			this.Kd2 = kd2;
+			this.encKey = encKey;
+		}
+	}
+
+	public static class UserAlreadyExists extends Exception {
+		String username;
+		UserAlreadyExists(String username) { this.username = username; }
+	}
+
+	public static class UserDoesntExist extends Exception {
+		String username;
+		UserDoesntExist(String username) { this.username = username; }
+	}
+
+	public static class DocumentDoesntExist extends Exception {
+		String name;
+		DocumentDoesntExist(String name) { this.name = name; }
+	}
+
+	public byte[] documentNameToID(String documentName) {
+		try {		
+			MessageDigest digest;
+			digest = MessageDigest.getInstance("SHA-256");
+			byte[] hash = digest.digest(documentName.getBytes());
+			return Arrays.copyOf(hash, idLength);
+			//			return documentName.getBytes();
+		} catch (NoSuchAlgorithmException e) {
+			return null;
+		}
+	}
+
+	public byte[] wordToID(String word) {
+		return documentNameToID(word);
 	}
 
 }
---
+++ src/test/java/org/crypto/sse/TestSSEwSU.java
@@ -0,0 +1,161 @@
+package org.crypto.sse;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.NoSuchProviderException;
+import java.security.spec.InvalidKeySpecException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import javax.crypto.NoSuchPaddingException;
+
+import org.crypto.sse.SSEwSU;
+import org.crypto.sse.SSEwSU.DocumentDoesntExist;
+import org.crypto.sse.SSEwSU.UserAlreadyExists;
+import org.crypto.sse.SSEwSU.UserDoesntExist;
+
+public class TestSSEwSU {
+
+	static String HELP_TEXT = "Commands: \n"
+			+ "> (e)nroll <username>\n"
+			+ "> (s)hare <document name> [<username>]+\n"
+			+ "> (u)nshare <document name> [<username>]+\n"
+			+ "> (q)uery <username> <keyword>\n"
+			+ "> [exit|quit]\n"
+			+ "> (h)elp\n";
+
+	public static void main(String[] args) 
+			throws InvalidKeyException, InvalidAlgorithmParameterException, 
+			NoSuchAlgorithmException, NoSuchProviderException, NoSuchPaddingException, 
+			InvalidKeySpecException, IOException {
+
+		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+		//
+		//		System.out.print("Enter your password : ");
+		//		String pass = input.readLine();
+		//
+		//		// generate keys
+		//		List<byte[]> listSK = IEX2Lev.keyGen(256, pass, "salt/salt", 100000);
+
+		System.out.print("Enter the relative path name of the folder that contains the files to make searchable: ");
+		String pathName = input.readLine();
+
+		ArrayList<File> listOfFile = new ArrayList<File>();
+		TextProc.listf(pathName, listOfFile);
+
+		TextProc.TextProc(false, pathName);
+
+		// Construction of the global multi-map
+		System.out.println("\nBeginning of Encrypted Multi-map creation \n");
+
+		SSEwSU sse = new SSEwSU(TextExtractPar.lp1);
+		
+		System.out.println(HELP_TEXT + "\n");
+		System.out.println(TextExtractPar.lp1);
+
+		boolean isDone = false;
+		while (!isDone) {
+			System.out.print(">> ");
+			String command = input.readLine();
+			if (command == "\n")
+				continue;
+
+			String[] splitCommand = command.split(" ");
+			switch (splitCommand[0]) {
+			case "e":
+			case "enroll":
+				{
+					if (splitCommand.length < 2) {
+						System.out.println("Error: expected format: (e)nroll <username>");
+						break;
+					}
+					String username = splitCommand[1];
+					try {
+						sse.enroll(username);
+						System.out.println("Successfully registered new user: " + username);
+					} catch (UserAlreadyExists e) {
+						System.out.println("Error: cannot enroll " + username + " user already exists");
+					}
+				}
+				break;
+			case "s":
+			case "share":
+				{			
+					if (splitCommand.length < 3) {
+						System.out.println("Expected format: (s)hare <document name> [<username>]+");
+						break;
+					}
+					String documentName = splitCommand[1];
+					for (int u = 2; u < splitCommand.length; ++u) {
+						String username = splitCommand[u];
+						try {
+							sse.shareDoc(documentName, username);
+							System.out.println("Successfully Shared " + documentName + " with " + username);
+						} catch (UserDoesntExist e) {
+							System.out.println("Error: user " + username + " does not exist");
+						} catch (DocumentDoesntExist e) {
+							System.out.println("Error: document " + documentName + " does not exist");
+						}
+					}
+				}
+				break;
+			case "u":
+			case "unshare":
+				{
+					if (splitCommand.length < 3) {
+						System.out.println("Expected format: (u)nshare <document name> [<username>]+");
+						break;
+					}
+					String documentName = splitCommand[1];
+					for (int u = 2; u < splitCommand.length; ++u) {
+						String username = splitCommand[u];
+						try {
+							sse.unshareDoc(documentName, username);
+							System.out.println("Successfully unshared " + documentName + " with " + username);
+						} catch (UserDoesntExist e) {
+							System.out.println("Error: user " + username + " does not exist");
+						} catch (DocumentDoesntExist e) {
+							System.out.println("Error: document " + documentName + " does not exist");
+						}
+					}
+				}
+				break;
+			case "q":
+			case "query":
+				{
+					if (splitCommand.length < 3) {
+						System.out.println("Error: expected format: (q)uery <username> <keyword>");
+						break;
+					}
+					String username = splitCommand[1];
+					String keyword = splitCommand[2];
+	
+					Collection<String> documentNames = sse.query(username, keyword);
+					if (documentNames != null) {
+						System.out.println(documentNames.size() + " documents found: " + documentNames.toString());
+					}
+				}
+				break;
+			case "h":
+			case "help":
+				System.out.println(HELP_TEXT);
+				break;
+			case "exit": 
+			case "quit":
+				isDone = true;
+				break;
+			default:
+				System.out.println("Error: Unknown command " + splitCommand[0]);
+				break;
+			}
+
+		}
+	}
+
+}
